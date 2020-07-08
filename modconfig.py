ac6111dd98372087dea16b16c33451be185e3ab0
Kirill Klenov <horneds@gmail.com>
2020-07-08 14:53:52 +0300

    feat: fallbacks and import path from env

--- modconfig.py
+++ modconfig.py
@@ -14,12 +14,10 @@ logger = logging.getLogger('scfg')
 
 class Config:
 
-    def __init__(self, mod, **scope):  # noqa
-        if mod:
-            try:
-                if isinstance(mod, str):
-                    mod = import_module(mod)
-
+    def __init__(self, *mods, **scope):  # noqa
+        if mods:
+            mod = import_mod(*mods)
+            if mod:
                 for name in dir(mod):
                     if name.startswith('_'):
                         continue
@@ -30,28 +28,51 @@ class Config:
 
                     scope.setdefault(name, value)
 
-            except ImportError:
-                logger.error('Invalid configuration module given: %s', mod)
-
+        scope = update_from_env(scope)
         self.__dict__.update(scope)
-
-        for name in os.environ:
-            if not hasattr(self, name):
-                continue
-
-            value = os.environ[name]
-            value_type = type(getattr(self, name))
-            try:
-                value = json.loads(value)
-            except ValueError:
-                pass
-
-            try:
-                value = value_type(value)
-            except (ValueError, TypeError):
-                continue
-
-            setattr(self, name, value)
 
     def __repr__(self):
         return "<Config %r>" % sorted(name for name in self.__dict__ if not name.startswith('_'))
+
+
+def import_mod(mod, *fallback):
+    """Load a module from python import path or environment."""
+    fallback = list(fallback)
+    while mod:
+        try:
+            if isinstance(mod, str):
+                if mod.startswith('ENV:'):
+                    mod = os.environ[mod[4:]]
+
+                mod = import_module(mod)
+
+            break
+
+        except (ImportError, KeyError):
+            logger.error('Invalid configuration module given: %s', mod)
+            mod = fallback and fallback.pop(0)
+
+    return mod
+
+
+def update_from_env(scope):
+    """Update the fiven configuration scope from OS Environment."""
+    for name in os.environ:
+        if name not in scope:
+            continue
+
+        value = os.environ[name]
+        value_type = type(scope[name])
+        try:
+            value = json.loads(value)
+        except ValueError:
+            pass
+
+        try:
+            value = value_type(value)
+        except (ValueError, TypeError):
+            continue
+
+        scope[name] = value
+
+    return scope
