ac7a9334b04d5e44dd545a3fe67136e85ff7437f
Kirill Klenov <horneds@gmail.com>
2020-12-25 20:55:36 +0300

    feat: wip

--- modconfig.py
+++ modconfig.py
@@ -21,29 +21,48 @@ class Config:
     def __init__(self, *mods, prefix='', update_from_env=True, **options):  # noqa
         self.__prefix__ = prefix
         if mods:
-            mod = import_mod(*mods)
-            if mod:
-                for name in dir(mod):
-                    if name.startswith('_'):
-                        continue
-
-                    value = getattr(mod, name)
-                    if isclass(value) or ismodule(value) or isbuiltin(value):
-                        continue
-
-                    options.setdefault(name, value)
+            self.import_mods(*mods)
 
         self.__dict__.update(options)
 
         if update_from_env:
             self.update_from_env()
 
+    def import_mods(self, mod, *fallback):
+        """Load a module from the given python path or environment."""
+        fallback = list(fallback)
+        while mod:
+            try:
+                if isinstance(mod, str):
+                    if mod.lower().startswith('env:'):
+                        mod = os.environ[mod[4:]]
+
+                    mod = import_module(mod)
+
+                break
+
+            except (ImportError, KeyError):
+                logger.error('Invalid configuration module given: %s', mod)
+                mod = fallback and fallback.pop(0)
+        else:
+            return False
+
+        for name in dir(mod):
+            if name.startswith('_'):
+                continue
+
+            value = getattr(mod, name)
+            if isclass(value) or ismodule(value) or isbuiltin(value):
+                continue
+
+            self.__dict__[name] = value
+
     def update_from_env(self):
         """Update the configuration from environment variables."""
         prefix_length = len(self.__prefix__)
         for name in os.environ:
             cfgname = name[prefix_length:]
-            if cfgname.startswith('__') or cfgname not in self.__dict__:
+            if cfgname.startswith('_') or cfgname not in self.__dict__:
                 continue
 
             value = os.environ[name]
@@ -63,23 +82,3 @@ class Config:
     def __repr__(self):
         """Representation."""
         return "<Config %r>" % sorted(name for name in self.__dict__ if not name.startswith('_'))
-
-
-def import_mod(mod, *fallback):
-    """Load a module from python import path or environment."""
-    fallback = list(fallback)
-    while mod:
-        try:
-            if isinstance(mod, str):
-                if mod.lower().startswith('env:'):
-                    mod = os.environ[mod[4:]]
-
-                mod = import_module(mod)
-
-            break
-
-        except (ImportError, KeyError):
-            logger.error('Invalid configuration module given: %s', mod)
-            mod = fallback and fallback.pop(0)
-
-    return mod
