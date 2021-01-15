b39d0934e0a001eba443e5331d3f43a2737bf733
Kirill Klenov <horneds@gmail.com>
2021-01-15 17:08:09 +0300

    build(version): 0.4.2 -> 0.5.0

--- modconfig.py
+++ modconfig.py
@@ -7,7 +7,7 @@ from importlib import import_module
 from inspect import isclass, isbuiltin, ismodule, getmembers
 
 
-__version__ = "0.4.2"
+__version__ = "0.5.0"
 __license__ = "MIT"
 __all__ = 'Config',
 
